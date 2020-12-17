f7b63a911cf6b454b3267fa63d0c9c9c5f541ef7
Kendall Shook <28932874+Shookems@users.noreply.github.com>
2020-12-17 17:24:52 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+//string is just text wrapped in double quotes, single quotes, or back ticks.
+//Integers Floats
+//Intergers any whole number without quotes to do math operations. 
+//Floats any decimal point number without quotes to do math operations (+ add, - subtract, * multiply, / divide, % modulus)
+//Modulus returns the remainder
+//boolean returns a value of true or false
+"Kendall" + " Shook"
+console.log('Kendall')
+console.log('Shook')
+console.log('Kendall'+'Shook')
